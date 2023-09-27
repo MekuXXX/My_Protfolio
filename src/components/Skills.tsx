a2c198b0e8a62a68a909ff6be430eee944cb61da
MekuXXX <mo7malo81@gmail.com>
2023-09-27 04:32:07 +0300

    Fix preview button color

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,7 @@ export default function Skills({}: Props) {
   return (
     <section>
       <SectionHeading text={"Skills"} />
-      <h2>We be added soon</h2>
+      <h2>Will be added soon</h2>
       {/* <div className="w-full h-screen relative flex items-center justify-center rounded-[50%] bg-circular-light">
         <div className={"flex items-center justify-center rounded-full font-semibold bg-"}>
           Web

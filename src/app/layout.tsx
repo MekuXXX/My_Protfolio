fb3499d179aa863cf91214bb0215b1ed9d697693
MekuXXX <mo7malo81@gmail.com>
2023-09-29 04:26:47 +0300

    Add twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,15 @@ export const metadata: Metadata = {
     ],
     type: "website",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Mohamed Portfolio",
+    description:
+      "Mohamed is a web designer with experience in field. Their portfolio showcases their skills in HTML, CSS, JavaScript, React, Node, etc... They are passionate about creating user-friendly and visually appealing websites. He have experience with SEO. Mohamed is looking for a challenging web design role where they can use their skills and experience to create innovative and effective websites.",
+    images: [
+      "https://imgs.search.brave.com/kcMBoPfE6uDaGrWJAlE1f4DMnnZjpQ_XAb28PWvYStY/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTc1/NDMxOTQ3L3Bob3Rv/L3BvcnRmb2xpby5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/OXBiMEFxY1dxaktJ/MmJKSl9wUmlkWnli/NWNLeHBkU1ZCNmRn/WU9wREx3ST0",
+    ],
+  },
 };
 
 export default function RootLayout({
